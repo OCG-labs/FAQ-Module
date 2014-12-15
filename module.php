75f37ad478b2be6a0cb01fa492903f2a9e40728e
Bryan Haskin <bhhaskin@users.noreply.github.com>
2014-12-15 10:50:28 -0800

    updateds

--- module.php
+++ module.php
@@ -1,23 +1,71 @@
 <?php
 /**
- * Load backstretch.js
+ * FAQ
  * @package Wordpress
  * @subpackage one-theme
  * @since 1.0
  * @author Matthew Hansen
  */
 
-require_once dirname( __FILE__ ) . 'fag.php';
+ namespace oneTheme;
 
-if( !function_exists( 'ot_load_faq_js' ) ) :
-  function ot_load_faq_js() {
-    if(!is_child_theme()){
-      wp_register_script( 'faq_js', get_template_directory_uri().'/lib/modules/faq/js/faq.js', array( 'jquery' ), '1.0.0', true );
-  } else {
-      wp_register_script( 'faq_js', get_stylesheet_directory_uri().'/lib/modules/faq/js/faq.js', array( 'jquery' ), '1.0.0', true );
-  }
-    wp_enqueue_script( 'faq_js' );
-  }
-  add_action( 'init', 'ot_load_faq_js' );
+ require_once get_template_directory() . '/lib/module.class.php';
 
-endif;
+ class FAQ extends Module {
+     use assetManager;
+
+     public function init(){
+         $this->customPostType();
+         add_shortcode("faq", array($this, 'shortCode'));
+     }
+
+     public function customPostType() {
+         $this->faq = new \Super_Custom_Post_Type('faq_cpt', 'FAQ', 'Frequently Asked Questions');
+         $this->faq->set_icon('list');
+     }
+
+     public function shortCode($content = null) {
+         global $wp_query;
+         $temp = $wp_query;
+         $wp_query= null;
+         $mypost = array(
+             'post_type' => 'faq_cpt',
+             'orderby' => 'menu_order',
+             'posts_per_page' => '-1',
+             'order' => 'ASC'
+         );
+
+         $wp_query = new \WP_Query( $mypost );
+
+         ob_start();
+         ?>
+         <?php $i = 1; ?>
+         <div class="panel-group" id="accordion">
+             <?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+                 <div>
+                     <div>
+                         <h4 class="panel-title">
+                             <a data-toggle="collapse" data-parent="#accordion" href="#collapse<?php echo $i; ?>">
+                                 <i class="indicator fa fa-caret-right fa-lg"></i> <?php the_title(); ?>
+                             </a>
+                         </h4>
+                     </div>
+                     <div id="collapse<?php echo $i; ?>" class="panel-collapse collapse">
+                         <div class="panel-body">
+                             <?php the_content(); ?>
+                         </div>
+                     </div>
+                 </div>
+                 <?php $i++; ?>
+             <?php endwhile; ?>
+         </div>
+
+         <?php $wp_query = null; $wp_query = $temp;
+         $content = ob_get_contents();
+         ob_end_clean();
+         return $content;
+     }
+
+ }
+
+ new FAQ();
